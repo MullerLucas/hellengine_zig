a46fe37485edfd9c2f822837d833e7c03477e39d
helloki <helloki@mailbox.org>
2023-09-17 16:21:02 +0200

    feat (engine | collections): implement basic chunk data structure

--- src/engine/collections.zig
+++ src/engine/collections.zig
@@ -1,2 +1,3 @@
 pub const SlotArray  = @import("./collections/slot_array.zig");
 pub const StackArray = @import("./collections/stack_array.zig").StackArray;
+pub const Chunk      = @import("./collections/chunk.zig").Chunk;
